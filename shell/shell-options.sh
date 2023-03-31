c036bd58f2ddf7fa18402c4dba612f9c1c9d1fd3
Jamaal Kirkpatrick <kjamaal81@gmail.com>
2023-03-31 10:02:48 -0700

    Update shell-options.sh

--- shell/shell-options.sh
+++ shell/shell-options.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-t some test parameter] [-p some other test parameter]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 \
+[-t some test parameter] \
+[-p some other test parameter]" 1>&2; exit 1; }
 
 while getopts ":t:p:" opt; do
     case "${opt}" in
